99530909d7d341766c6a8bfe7f6f71e2406849f0
aleksey-10 <alexcare1337@gmail.com>
2020-07-06 22:25:37 +0300

    Updates

---
+++ precache-manifest.c3838acfb01910e45fe918604cc76087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8048b04e827864978d21dc2fc52ada76",
+    "url": "/tt_todo_1/index.html"
+  },
+  {
+    "revision": "f479144c33ec06d00af7",
+    "url": "/tt_todo_1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4bc7b374587e5db6020b",
+    "url": "/tt_todo_1/static/css/main.f7c5864d.chunk.css"
+  },
+  {
+    "revision": "f479144c33ec06d00af7",
+    "url": "/tt_todo_1/static/js/2.83963db9.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/tt_todo_1/static/js/2.83963db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc7b374587e5db6020b",
+    "url": "/tt_todo_1/static/js/main.d32cf31a.chunk.js"
+  },
+  {
+    "revision": "a369e18ca19a721b15f6",
+    "url": "/tt_todo_1/static/js/runtime-main.f374ce12.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tt_todo_1/precache-manifest.c52645d83c83acb04be1da64e35a810f.js"
+  "/tt_todo_1/precache-manifest.c3838acfb01910e45fe918604cc76087.js"
 );
 
 self.addEventListener('message', (event) => {
